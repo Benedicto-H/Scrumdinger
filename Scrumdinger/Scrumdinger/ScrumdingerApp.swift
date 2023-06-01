bab303f81d5c9e6809e834a14a4730bb6a7dfae9
JP <wlsvy1005@gmail.com>
2023-06-01 17:18:49 +0900

    Initialize Scrumdinger

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 홍진표 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
