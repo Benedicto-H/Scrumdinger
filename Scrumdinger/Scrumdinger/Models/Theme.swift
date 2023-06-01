49085417484f3416ec0dddf27c56a3da9b0c377d
JP <wlsvy1005@gmail.com>
2023-06-01 20:32:30 +0900

    [Feat]: Views - Creating a card view 완성

---
+++ Scrumdinger/Scrumdinger/Models/Theme.swift
@@ -0,0 +1,51 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by 홍진표 on 2023/06/01.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    // MARK: - Computed-Props
+    var accentColor: Color {
+        
+        get {
+            switch self {
+            case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow:
+                
+                return .black
+                
+            case .indigo, .magenta, .navy, .oxblood, .purple:
+                
+                return .white
+            }
+        }
+    }
+    
+    var mainColor: Color {
+        
+        get {
+            return Color(rawValue)
+        }
+    }
+}
