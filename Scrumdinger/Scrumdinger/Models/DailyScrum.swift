75948a474d707718c3c8c2e3ab08b10fe297c8a5
JP <wlsvy1005@gmail.com>
2023-06-02 19:10:46 +0900

    [Feat]: Views - Displaying data in a list 완성

--- Scrumdinger/Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Scrumdinger/Models/DailyScrum.swift
@@ -7,31 +7,42 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
     
     // MARK: - Stored-Props
+    let id: UUID
     var title: String
     var attendees: [String]
     var lengthInMinutes: Int
     var theme: Theme
+    
+    // MARK: - Init
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
 }
 
 extension DailyScrum {
     
-    // MARK: - Singleton Instance
-    static let sampleData: [DailyScrum] =
-    [
-        DailyScrum(title: "Design",
-                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
-                   lengthInMinutes: 10,
-                   theme: .yellow),
-        DailyScrum(title: "App Dev",
-                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
-                   lengthInMinutes: 5,
-                   theme: .orange),
-        DailyScrum(title: "Web Dev",
-                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
-                   lengthInMinutes: 5,
-                   theme: .poppy)
-    ]
+    // MARK: - Computed-Prop (-> Singleton Instance)
+    static var sampleData: [DailyScrum] {
+        [
+            DailyScrum(title: "Design",
+                       attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                       lengthInMinutes: 10,
+                       theme: .yellow),
+            DailyScrum(title: "App Dev",
+                       attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                       lengthInMinutes: 5,
+                       theme: .orange),
+            DailyScrum(title: "Web Dev",
+                       attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                       lengthInMinutes: 5,
+                       theme: .poppy)
+        ]
+    }
 }
